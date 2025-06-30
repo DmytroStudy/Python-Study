f0bb84ed9caa04cfaf27dc0289614e0659faed41
DmytroStudy <dmytrostud@gmail.com>
2025-06-30 14:09:41 +0200

    First program

---
+++ first.py
@@ -0,0 +1,16 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+
+print("Hello,", name)
+
+#Checking age
+adult = False
+if age >= 18: adult = True
+
+if adult == True: print("You are an adult")
+else: print("You are not an adult")
+
+'''
+That's
+all
+'''
